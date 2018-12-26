2e238847cf3be998b5fa262f1507906b06495db4
zavr-1 <zavr-1@users.noreply.github.com>
2018-12-26 23:57:23 +0300

    Create es.js

---
+++ es.js
@@ -0,0 +1,33 @@
+
+/** @type {import('koa').Middleware} */
+const es = async (ctx, next) => {
+  let e; try { await next() } catch (err) { e = err }
+  const {
+    appName: app, request: { ip, path }, headers: { cookie, ...headers },
+    client, status,
+  } = ctx
+  const body = {
+    app,
+    ip,
+    path,
+    headers,
+    status,
+    date: new Date(),
+  }
+  const d = new Date()
+  const y = d.getFullYear()
+  const m = d.getMonth() + 1
+
+  const id = sync(18)
+  client.create({
+    index: `technation.sucks-${y}.${m}`,
+    type: 'hit',
+    id,
+    body,
+  }).catch(() => {
+    console.log('ES PUT ERROR')
+  })
+  console.log(path,id)
+
+  if (e) throw e
+}
