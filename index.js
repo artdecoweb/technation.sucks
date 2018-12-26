1a5e21a7014c65e0285494688d58033ce3f90ea8
zavr-1 <zavr-1@users.noreply.github.com>
2018-12-26 23:57:09 +0300

    Update index.js

--- index.js
+++ index.js
@@ -2,44 +2,11 @@ const idio = require('@idio/core')
 const { Client } = require('elasticsearch');
 const { sync } = require('uid-safe');
 const { readBuffer } = require('@wrote/read')
+const es = require('./es')
 
 const client = new Client({
   host: '13.81.242.64:9200',
 })
-
-
-/** @type {import('koa').Middleware} */
-const es = async (ctx, next) => {
-  let e; try { await next() } catch (err) { e = err }
-  const {
-    appName: app, request: { ip, path }, headers: { cookie, ...headers },
-    client, status,
-  } = ctx
-  const body = {
-    app,
-    ip,
-    path,
-    headers,
-    status,
-    date: new Date(),
-  }
-  const d = new Date()
-  const y = d.getFullYear()
-  const m = d.getMonth() + 1
-
-  const id = sync(18)
-  client.create({
-    index: `technation.sucks-${y}.${m}`,
-    type: 'hit',
-    id,
-    body,
-  }).catch(() => {
-    console.log('ES PUT ERROR')
-  })
-  console.log(path,id)
-
-  if (e) throw e
-}
 
 ;(async () => {
   await client.ping()
