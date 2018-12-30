704b9b5fb7ae5bd643ede730f272bc8d1a5b0a31
Anton <anton@adc.sh>
2018-12-31 01:56:33 +0300

    static

--- src/app.js
+++ src/app.js
@@ -1,19 +1,22 @@
 import idio from '@idio/core'
 import { Client } from 'elasticsearch'
-import { readBuffer } from '@wrote/read'
 import { c } from 'erte'
 import es from './es'
 
 const host = `${process.env.ELASTIC}:9200`
+const PROD = process.env.NODE_ENV == 'production'
 
 const client = new Client({
   host,
 })
 
+const MIN = 1000 * 60
+const HOUR = 60 * MIN
+const DAY = 24 * HOUR
+
 ;(async () => {
   await client.ping()
   console.log('Connected to %s', c(host, 'red'))
-  const img = await readBuffer('reflex.jpg')
   const { app, url } = await idio({
     async setup(ctx, next) {
       ctx.client = client
@@ -21,17 +24,16 @@ const client = new Client({
       ctx.appName = 'technation.sucks'
       await next()
     },
+    es,
     compress: {
       use: true,
     },
-    es,
-    async img(ctx, next) {
-      if (ctx.path == '/reflex.jpg') {
-        ctx.type = 'image/jpeg'
-        ctx.body = img
-      } else {
-        await next()
-      }
+    static: {
+      use: true,
+      root: 'static',
+      config: {
+        maxage: PROD ? 7 * DAY : 0,
+      },
     },
     redirect(ctx) {
       ctx.redirect('https://www.technation.sucks')
