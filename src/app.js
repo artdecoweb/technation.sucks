d39c0a94999aa8d702626661580eb3bfa7439782
Anton <anton@adc.sh>
2019-01-02 02:59:33 +0300

    linkedin

--- src/app.js
+++ src/app.js
@@ -2,7 +2,10 @@ import idio from '@idio/core'
 import { Client } from 'elasticsearch'
 import { c } from 'erte'
 import staticCache from 'koa-static-cache'
+import linkedIn, { query, getUser, linkedInButton } from '@idio/linkedin'
+import dotenv from '@demimonde/dotenv'
 import es from './es'
+dotenv()
 
 const host = `${process.env.ELASTIC}:9200`
 const PROD = process.env.NODE_ENV == 'production'
@@ -14,10 +17,13 @@ const client = new Client({
 ;(async () => {
   await client.ping()
   console.log('Connected to %s', c(host, 'red'))
-  const sc = staticCache('static', {
+  const files = {}
+  const opt = {
     gzip: true,
-  })
-  const { url } = await idio({
+  }
+  const sc = staticCache('static', opt, files)
+  staticCache('node_modules/@idio/linkedin/img', opt, files)
+  const { app, router, url } = await idio({
     logger: {
       use: PROD,
     },
@@ -29,9 +35,47 @@ const client = new Client({
     },
     es,
     sc,
-    redirect(ctx) {
-      ctx.redirect('https://www.technation.sucks')
-    },
+    session: { use: true, keys: [process.env.SESSION_KEY] },
   }, { port: process.env.PORT })
+  linkedIn(router, {
+    client_id: process.env.LINKEDIN_ID,
+    client_secret: process.env.LINKEDIN_SECRET,
+    scope: 'r_liteprofile,r_basicprofile',
+    async finish(ctx, token, user) {
+      const { positions: { values: pos } } = await query({
+        token,
+        path: 'people/~:(positions)',
+        version: 'v1',
+      })
+      const positions = pos.map(({
+        title,
+        company: { id, name },
+        location: { name: location } ,
+      }) => {
+        return {
+          id, name, title,
+          location: location.replace(/,\s*$/, ''),
+        }
+      })
+      ctx.session.positions = positions
+      ctx.session.token = token
+      ctx.session.user = getUser(user)
+      ctx.redirect('/comments')
+    },
+  })
+  router.get('/comments', async (ctx, next) => {
+    const { button, idioCommon, style } = await linkedInButton()
+    ctx.body = `<style>
+      ${idioCommon}
+      ${style}
+    </style>
+    ${button}
+    `
+  })
+  app.use(router.routes())
+  app.use((ctx) => {
+    ctx.redirect('https://www.technation.sucks')
+  })
+
   console.log('Started on %s', c(url, 'green'))
 })()
