917946d7c1d056daeb92cda7a447a809d9062481
Anton <anton@adc.sh>
2019-01-06 15:14:21 +0300

    a

--- src/app.js
+++ src/app.js
@@ -71,7 +71,26 @@ const client = new Client({
     `
   })
   router.get('/counter.svg', counter)
+  router.get('/linkedin1', (ctx) => {
+    if (!/LinkedInBot/.test(ctx.headers['user-agent'])) {
+      ctx.redirect('https://www.technation.sucks/#check')
+      return
+    }
+    ctx.body = `<!doctype html>
+<html lang="en">
+  <head>
+    <meta property="og:title" content="Tech Nation Sucks: How the Senior Manager thought they could lie to the Home Office by making the decision on my application themselves." />
+    <meta name="author" content="Art Deco" />
+    <meta property="og:image" content="https://raw.githubusercontent.com/artdecoweb/www.technation.sucks/master/linkedin/key-linkedin.png" />
+  </head>
+</html>`
+  })
   app.use(router.routes())
+  // app.use((ctx) => {
+  //   if(ctx.path.startsWith('/linkedin/')) {
+  //     const s = ctx.path.replace('/linkedin/', '')
+  //   }
+  // })
   app.use((ctx) => {
     ctx.redirect('https://www.technation.sucks')
   })
