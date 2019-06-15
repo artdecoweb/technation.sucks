cc891b6bad1d6c75759608778853fe2fadf011b3
Anton <anton@adc.sh>
2019-06-15 04:30:01 +0300

    referrer

--- routes/post/diagnostics.js
+++ routes/post/diagnostics.js
@@ -3,15 +3,16 @@ import uniqid from 'uniqid'
  * @type {import('../../').Middleware}
  */
 export default async function (ctx) {
-  const { time, url, scroll } = ctx.req.body
+  const { time, url, scroll, referrer } = ctx.req.body
   await ctx.client.create({
     index: 'telemetry.technation.sucks',
     id: uniqid(),
     type: 'unload',
     body: { time, scroll, date: new Date(), url,
       'user-agent': ctx.request.headers['user-agent'],
-      'referrer': ctx.request.headers['referer'],
+      'referer': ctx.request.headers['referer'],
       ip: ctx.request.ip,
+      referrer,
     },
   })
   ctx.status = 200
